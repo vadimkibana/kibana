fc8b8409c0248713aa13bcbf2213e2c02babebe9
vadimkibana <vadimkibana@gmail.com>
2023-02-28 13:36:44 +0100

    add mappings

---
+++ src/plugins/content_management/server/event_stream/mappings.ts
@@ -0,0 +1,40 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+export const mappings = {
+  dynamic: false,
+  properties: {
+    '@timestamp': {
+      type: 'date',
+    },
+    subjectType: {
+      type: 'keyword',
+      ignore_above: 1024,
+    },
+    subjectId: {
+      type: 'keyword',
+      ignore_above: 1024,
+    },
+    predicate: {
+      type: 'keyword',
+      ignore_above: 1024,
+    },
+    payload: {
+      type: 'object',
+      dynamic: true,
+    },
+    objectType: {
+      type: 'keyword',
+      ignore_above: 1024,
+    },
+    objectId: {
+      type: 'keyword',
+      ignore_above: 1024,
+    },
+  },
+};
