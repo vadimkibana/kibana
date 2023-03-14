ba818c25b4225d4f1ecb3f87430bd414ee1692d1
vadimkibana <vadimkibana@gmail.com>
2023-03-14 19:34:29 +0100

    add flush method and jsdocs

--- src/plugins/content_management/server/event_stream/event_stream_service.ts
+++ src/plugins/content_management/server/event_stream/event_stream_service.ts
@@ -28,29 +28,29 @@ export interface EventStreamSetup {
 export class EventStreamService {
   public client?: EventStreamClient;
 
-  public readonly flush = (events: EventStreamEvent[]): void => {
-    if (!this.client) throw new Error('EventStreamClient not initialized.');
-    this.client.writeEvents(events)
-      .catch((error) => {
-        const { logger } = this.ctx;
-
-        logger.error('Failed to write events to Event Stream.');
-        logger.error(error);
-      });
-  };
-
   readonly #buffer = new TimedItemBuffer<EventStreamEvent>({
-    onFlush: this.flush,
     flushOnMaxItems: 100,
     maxItemAge: 250,
+    onFlush: (events: EventStreamEvent[]): void => {
+      if (!this.client) throw new Error('EventStreamClient not initialized.');
+      this.client.writeEvents(events)
+        .catch((error) => {
+          const { logger } = this.ctx;
+  
+          logger.error('Failed to write events to Event Stream.');
+          logger.error(error);
+        });
+    },
   });
 
   constructor(private readonly ctx: EventStreamInitializerContext) {}
 
+  /** Called during "setup" plugin life-cycle. */
   public setup({ core }: EventStreamSetup): void {
     this.client = this.ctx.clientFactory.create(core);
   }
 
+  /** Called during "start" plugin life-cycle. */
   public start(): void {
     const { logger } = this.ctx;
 
@@ -72,12 +72,27 @@ export class EventStreamService {
     return this.client;
   }
 
+  /**
+   * Read latest events from the Event Stream.
+   * 
+   * @param limit Number of events to return. Defaults to 100.
+   * @returns Latest events from the Event Stream.
+   */
   public async tail(limit: number = 100): Promise<EventStreamEvent[]> {
     const client = this.#getClient();
 
     return await client.tail(limit);
   }
 
+  /**
+   * Queues an event to be written to the Event Stream. The event is appended to
+   * a buffer and written to the Event Stream periodically.
+   * 
+   * Events are flushed once the buffer reaches 100 items or 250ms has passed,
+   * whichever comes first. To force a flush, call `.flush()`.
+   * 
+   * @param event Event to add to the Event Stream.
+   */
   public addEvent(event: Optional<EventStreamEvent, 'time'>): void {
     const completeEvent: EventStreamEvent = {
       ...event,
@@ -87,9 +102,21 @@ export class EventStreamService {
     this.#buffer.write(completeEvent);
   }
 
+  /**
+   * Same as `.addEvent()` but accepts an array of events.
+   * 
+   * @param events Events to add to the Event Stream.
+   */
   public addEvents(events: Optional<EventStreamEvent, 'time'>[]): void {
     for (const event of events) {
       this.addEvent(event);
     }
+  }
+
+  /**
+   * Flushes the event buffer, writing all events to the Event Stream.
+   */
+  public flush(): void {
+    this.#buffer.flush();
   }
 }
