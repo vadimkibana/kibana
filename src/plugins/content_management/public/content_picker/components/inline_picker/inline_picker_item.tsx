a286f45ebf4b3f417160e24abe42141b9961bd6b
vadimkibana <vadimkibana@gmail.com>
2023-12-20 03:23:04 +0100

    show titles of loaded items

--- src/plugins/content_management/public/content_picker/components/inline_picker/inline_picker_item.tsx
+++ src/plugins/content_management/public/content_picker/components/inline_picker/inline_picker_item.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
+import useObservable from 'react-use/lib/useObservable';
 import {EuiListGroupItem} from '@elastic/eui';
 import {useContentPicker} from '../../context/state';
-import {useBehaviorSubject} from '../../hooks/use_behavior_subject';
 import type {ContentId} from '../../types';
 
 export interface Props {
@@ -10,15 +10,25 @@ export interface Props {
 
 export const InlinePickerItem: React.FC<Props> = ({id}) => {
   const {state} = useContentPicker();
-  // state.services.client.get$({contentTypeId: id}})
-  const list = useBehaviorSubject(state.getQueryResults(''));
+  const item$ = React.useMemo(() => state.services.client.get$({contentTypeId: id[0], id: id[1]}), [id[0], id[1]]);
+  const item = useObservable(item$);
+  
+  if (item?.status !== 'success') {
+    return (
+      <EuiListGroupItem
+        iconType="loading"
+        label={''}
+        onClick={() => {}}
+      />
+    );
+  }
 
   return (
     <EuiListGroupItem
-        iconType="bullseye"
-        label={id[0] + ':' + id[1]}
-        onClick={() => {}}
-        // isActive
-      />
+      iconType="bullseye"
+      label={(item.data as any).item.attributes.title}
+      onClick={() => {}}
+      // isActive
+    />
   );
 };
