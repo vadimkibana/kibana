555cfafe9afa3befc4c88e306af5cc4027da941c
vadimkibana <vadimkibana@gmail.com>
2024-04-15 13:48:26 +0200

    add tabbing to connection details

--- packages/cloud/connection_details/connection_details.tsx
+++ packages/cloud/connection_details/connection_details.tsx
@@ -7,15 +7,60 @@
  */
 
 import * as React from 'react';
+import { EuiTab, EuiTabs } from '@elastic/eui';
 import { useConnectionDetails } from './context';
 import { EndpointsTab } from './tabs/endpoints_tab';
+import { i18n } from '@kbn/i18n';
 
 export const ConnectionDetails: React.FC = () => {
+  type TabID = 'endpoints' | 'apiKeys';
+  type Tab = [
+    id: TabID,
+    name: string,
+    content: React.ReactNode,
+  ];
+
   const ctx = useConnectionDetails();
+  const [tab, setTab] = React.useState<TabID>('endpoints');
+
+  const tabs: Tab[] = [];
+  
+  if (ctx.endpoints) {
+    tabs.push([
+      'endpoints',
+      i18n.translate('cloud.connectionDetails.tab.endpoints', {
+        defaultMessage: 'Endpoints',
+      }),
+      <EndpointsTab />,
+    ]);
+  }
+
+  if (ctx.apiKeys) {
+    tabs.push([
+      'apiKeys',
+      i18n.translate('cloud.connectionDetails.tab.apiKeys', {
+        defaultMessage: 'API Keys',
+      }),
+      null,
+    ]);
+  }
+
+  if (tabs.length === 0) {
+    return null;
+  }
 
   return (
     <>
-      {!!ctx.endpoints && <EndpointsTab />}
+      <EuiTabs>
+        {tabs.map(([id, name]) => (
+          <EuiTab
+            key={id}
+            onClick={() => setTab(id)}
+            isSelected={tab === id}
+          >{name}</EuiTab>  
+        ))}
+      </EuiTabs>
+      {tabs.find(([id]) => id === tab)?.[2] || null}
     </>
   );
 };
