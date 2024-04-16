a83ff802c85eecaa5eaa8d346be643b61ac4180f
vadimkibana <vadimkibana@gmail.com>
2024-04-16 11:35:30 +0200

    improve copy field component look

--- packages/cloud/connection_details/components/copy_input/copy_input.tsx
+++ packages/cloud/connection_details/components/copy_input/copy_input.tsx
@@ -8,11 +8,12 @@
 
 import * as React from 'react';
 import {
-  EuiFieldText,
   EuiCopy,
   EuiButtonIcon,
   EuiFlexGroup,
   EuiFlexItem,
+  EuiPanel,
+  EuiText,
 } from '@elastic/eui';
 
 export interface EndpointUrlProps {
@@ -23,22 +24,31 @@ export const CopyInput: React.FC<EndpointUrlProps> = ({
   value,
 }) => {
   return (
-    <EuiFlexGroup gutterSize="s">
-      <EuiFlexItem>
-        <EuiFieldText
-          value={value}
-          fullWidth
-          disabled
-          data-test-subj="copyInputField"
-        />
-      </EuiFlexItem>
-      <EuiFlexItem grow={false}>
-        <EuiCopy textToCopy={value}>
-          {(copy) => (
-            <EuiButtonIcon onClick={copy} iconType="copyClipboard" display="base" size="m" />
-          )}
-        </EuiCopy>
-      </EuiFlexItem>
-    </EuiFlexGroup>
+    <EuiPanel
+      borderRadius="none"
+      hasShadow={false}
+      color={'subdued'}
+      grow={false}
+    >
+      <EuiFlexGroup gutterSize="s">
+        <EuiFlexItem>
+          <EuiText
+            size={'s'}
+            color={'subdued'}
+            style={{wordBreak: 'break-all'}}
+            data-test-subj="copyInputValue"
+          >
+            {value}
+          </EuiText>
+        </EuiFlexItem>
+        <EuiFlexItem grow={false}>
+          <EuiCopy textToCopy={value}>
+            {(copy) => (
+              <EuiButtonIcon onClick={copy} iconType="copyClipboard" size="m" color={'text'} />
+            )}
+          </EuiCopy>
+        </EuiFlexItem>
+      </EuiFlexGroup>
+    </EuiPanel>
   );
 };
