ecde28585f8990e8f1fd66faa1d5098af7286278
vadimkibana <vadimkibana@gmail.com>
2024-11-27 13:05:45 +0100

    abstract synth creation methods

--- packages/kbn-esql-ast/src/esql/expr.ts
+++ packages/kbn-esql-ast/src/esql/expr.ts
@@ -10,12 +10,12 @@
 import { ParseOptions } from '../parser';
 import { EsqlQuery } from '../query';
 import { firstItem } from '../visitor/utils';
-import { clearParserFields } from './helpers';
+import { clearParserFields, createSynthMethod } from './helpers';
 import { BasicPrettyPrinter } from '../pretty_print';
-import type { SynthGenerator, SynthTaggedTemplate, SynthTaggedTemplateWithOpts } from './types';
+import type { SynthGenerator, SynthSerializer } from './types';
 import type { ESQLAstExpression } from '../types';
 
-const generate: SynthGenerator = (
+const generator: SynthGenerator<ESQLAstExpression> = (
   src: string,
   { withFormatting = true, ...rest }: ParseOptions = {}
 ): ESQLAstExpression => {
@@ -29,53 +29,8 @@ const generate: SynthGenerator = (
   return expression;
 };
 
-const expressionTag: SynthTaggedTemplateWithOpts = (opts?: ParseOptions) => {
-  return (template: TemplateStringsArray, ...params: Array<ESQLAstExpression | string>) => {
-    let src = '';
-    const length = template.length;
-    for (let i = 0; i < length; i++) {
-      src += template[i];
-      if (i < params.length) {
-        const param = params[i];
-        if (typeof param === 'string') src += param;
-        else src += BasicPrettyPrinter.expression(param);
-      }
-    }
-    return generate(src, opts);
-  };
+const serializer: SynthSerializer<ESQLAstExpression> = (node: ESQLAstExpression) => {
+  return BasicPrettyPrinter.expression(node);
 };
 
-export const expr: SynthGenerator & SynthTaggedTemplate & SynthTaggedTemplateWithOpts = ((
-  ...args
-) => {
-  const [first] = args;
-
-  /**
-   * Usage as function:
-   *
-   * ```js
-   * expr('42');
-   * ```
-   */
-  if (typeof first === 'string') return generate(first, args[1] as ParseOptions);
-
-  /**
-   * Usage as tagged template:
-   *
-   * ```js
-   * expr`42`;
-   * ```
-   */
-  if (Array.isArray(first)) {
-    return expressionTag()(first as TemplateStringsArray, ...(args as any).slice(1));
-  }
-
-  /**
-   * Usage as tagged template, with ability to specify parsing options:
-   *
-   * ```js
-   * expr({ withFormatting: false })`42`;
-   * ```
-   */
-  return expressionTag(args[0] as ParseOptions);
-}) as SynthGenerator & SynthTaggedTemplate & SynthTaggedTemplateWithOpts;
+export const expr = createSynthMethod<ESQLAstExpression>(generator, serializer);
--- packages/kbn-esql-ast/src/esql/helpers.ts
+++ packages/kbn-esql-ast/src/esql/helpers.ts
@@ -9,6 +9,14 @@
 
 import { Builder } from '../builder';
 import { Walker, WalkerAstNode } from '../walker/walker';
+import type { ESQLProperNode } from '../types';
+import type {
+  SynthGenerator,
+  SynthMethod,
+  SynthSerializer,
+  SynthTaggedTemplateWithOpts,
+} from './types';
+import type { ParseOptions } from '../parser';
 
 export const clearParserFields = (ast: WalkerAstNode) => {
   Walker.walk(ast, {
@@ -16,4 +24,63 @@ export const clearParserFields = (ast: WalkerAstNode) => {
       Object.assign(node, Builder.parserFields({}));
     },
   });
+};
+
+export const createSynthMethod = <N extends ESQLProperNode>(
+  generator: SynthGenerator<N>,
+  serializer: SynthSerializer<N>
+): SynthMethod<N> => {
+  const templateStringTag: SynthTaggedTemplateWithOpts<N> = ((opts?: ParseOptions) => {
+    return (template: TemplateStringsArray, ...params: Array<N | string>) => {
+      let src = '';
+      const length = template.length;
+      for (let i = 0; i < length; i++) {
+        src += template[i];
+        if (i < params.length) {
+          const param = params[i];
+          if (typeof param === 'string') src += param;
+          else src += serializer(param);
+        }
+      }
+      return generator(src, opts);
+    };
+  }) as SynthTaggedTemplateWithOpts<N>;
+
+  const method: SynthMethod<N> = ((...args) => {
+    const [first] = args;
+
+    /**
+     * Usage as function:
+     *
+     * ```js
+     * expr('42');
+     * ```
+     */
+    if (typeof first === 'string') return generator(first, args[1] as ParseOptions);
+
+    /**
+     * Usage as tagged template:
+     *
+     * ```js
+     * expr`42`;
+     * ```
+     */
+    if (Array.isArray(first)) {
+      return templateStringTag()(
+        first as unknown as TemplateStringsArray,
+        ...(args as any).slice(1)
+      );
+    }
+
+    /**
+     * Usage as tagged template, with ability to specify parsing options:
+     *
+     * ```js
+     * expr({ withFormatting: false })`42`;
+     * ```
+     */
+    return templateStringTag(args[0] as ParseOptions);
+  }) as SynthMethod<N>;
+
+  return method;
 };
